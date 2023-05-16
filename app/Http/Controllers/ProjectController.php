2d50cc2a2d2c31f7c9e3d86326cb80eda15e1e50
rodhisz <rodhisz.45@gmail.com>
2023-05-16 10:31:23 +0700

    first commit

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'images' => 'required|array'
+        ]);
+
+        $images = [];
+
+        foreach ($data['images'] as $image) {
+            $fileName = uniqid() . '.' . $image->getClientOriginalExtension();
+            $image_path =  $image->storeAs('images', $fileName,'public');
+
+            array_push($images, $image_path);
+        }
+
+        $data['images'] = $images;
+
+        Project::create($data);
+        return redirect()->route('projects.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Project $project)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        //
+    }
+}
